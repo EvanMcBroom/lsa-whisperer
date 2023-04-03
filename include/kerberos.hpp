9202f2a2a7c85fe5e3f0f44250928df833694320
Evan McBroom <15305789+EvanMcBroom@users.noreply.github.com>
2023-04-03 08:38:17 -0400

    Added prototype for kerberos RetrieveEncodedTicket

--- include/kerberos.hpp
+++ include/kerberos.hpp
@@ -10,6 +10,40 @@
 #include <vector>
 
 namespace Kerberos {
+    enum class CacheOptions : ULONG {
+        Default = 0x0,
+        DontUseCache = 0x1,
+        UseCacheOnly = 0x2,
+        UseCredhandle = 0x4,
+        // NT 5.1
+        AsKerbCred = 0x8,
+        WithSecCred = 0x10,
+        // NT 6.0
+        CACHE_TICKET = 0x20,
+        // NT 6.1
+        MAX_LIFETIME = 0x40
+    };
+
+    enum class EncryptionType {
+        Null = 0,
+        DesCbcCrc = 1,
+        DesCbcMd4 = 2,
+        DesCbcMd5 = 3,
+        Aes128CtsHmacSha1_96 = 17,
+        Aes256CtsHmacSha1_96 = 18,
+        Rc4Md4 = -128,
+        Rc4Plain2 = -129,
+        Rc4Lm = -130,
+        Rc4sha = -131,
+        DesPlan = -132,
+        Rc4HmacOld = -133,
+        Rc4PlainOld = -134,
+        Rc4HmacOldExp = -135,
+        Rc4PlainOldExp = -136,
+        Rc4Plain = -140,
+        Rc4PlainExp = -141
+    };
+
     enum class PROTOCOL_MESSAGE_TYPE : ULONG {
         DebugRequest = 0,
         QueryTicketCache,
@@ -24,7 +58,6 @@ namespace Kerberos {
         AddBindingCacheEntry,
         SetPassword,
         SetPasswordEx,
-        AddExtraCredentialsMessage = 17,
         VerifyCredentials,
         QueryTicketCacheEx,
         PurgeTicketCacheEx,
@@ -48,8 +81,42 @@ namespace Kerberos {
         QueryS4U2ProxyCache,
         RetrieveKeyTab,
         RefreshPolicy,
-        PrintCloudKerberosDebugMessage
+        PrintCloudKerberosDebug
     };
+
+    enum class TicketFlags : ULONG {
+        Reserved1 = 0x00000001,
+        NameCanonicalize = 0x00010000,
+        EncPaRep = 0x00010000,
+        CNameInPaData = 0x00040000, // Only valid on NT 5.1
+        OkAsDelegate = 0x00040000,
+        HwAuthent = 0x00100000,
+        PreAuthent = 0x00200000,
+        Initial = 0x00400000,
+        Renewable = 0x00800000,
+        Invalid = 0x01000000,
+        PostDated = 0x02000000,
+        MayPostdate = 0x04000000,
+        Proxy = 0x08000000,
+        Proxiable = 0x10000000,
+        Forwarded = 0x20000000,
+        Forwardable = 0x40000000,
+        Feserved = 0x80000000
+    };
+
+    typedef struct _RETRIEVE_TKT_REQUEST {
+        PROTOCOL_MESSAGE_TYPE MessageType{ PROTOCOL_MESSAGE_TYPE::RetrieveEncodedTicket };
+        LUID LogonId;
+        UNICODE_STRING TargetName;
+        ULONG TicketFlags;
+        ULONG CacheOptions;
+        LONG EncryptionType;
+        SecHandle CredentialsHandle{ 0 };
+    } RETRIEVE_TKT_REQUEST, *PRETRIEVE_TKT_REQUEST;
+
+    typedef struct _RETRIEVE_TKT_RESPONSE {
+        KERB_EXTERNAL_TICKET Ticket;
+    } RETRIEVE_TKT_RESPONSE, *PRETRIEVE_TKT_RESPONSE;
 
     typedef struct _QUERY_TKT_CACHE_REQUEST {
         PROTOCOL_MESSAGE_TYPE MessageType{ PROTOCOL_MESSAGE_TYPE::QueryTicketCache };
@@ -62,7 +129,7 @@ namespace Kerberos {
 
         // A subset of the supported functions in Kerberos
         bool QueryTicketCache(PLUID luid) const;
-        // bool KerbChangeMachinePassword(const std::wstring username, const std::wstring domain, CacheLookupCredType type, const std::string credential) const;
+        bool RetrieveEncodedTicket(PLUID luid, const std::wstring& targetName, TicketFlags flags, CacheOptions options, EncryptionType type) const;
 
     protected:
         std::shared_ptr<Lsa> lsa;
--- source/kerberos.cpp
+++ source/kerberos.cpp
@@ -23,6 +23,49 @@ namespace Kerberos {
         }
         return result;
     }
+
+    bool Proxy::RetrieveEncodedTicket(PLUID luid, const std::wstring& targetName, TicketFlags flags, CacheOptions options, EncryptionType type) const {
+        std::vector<byte> requestBytes(sizeof(RETRIEVE_TKT_REQUEST) + (targetName.size() * sizeof(wchar_t)), 0);
+        auto request{ reinterpret_cast<PRETRIEVE_TKT_REQUEST>(requestBytes.data()) };
+        request->MessageType = PROTOCOL_MESSAGE_TYPE::RetrieveEncodedTicket;
+        request->LogonId.LowPart = luid->LowPart;
+        request->LogonId.HighPart = luid->HighPart;
+        request->TargetName.Length = targetName.size() * sizeof(wchar_t);
+        request->TargetName.MaximumLength = request->TargetName.Length;
+        auto buffer{ reinterpret_cast<wchar_t*>(request + 1) };
+        std::memcpy(buffer, targetName.data(), targetName.length() * sizeof(wchar_t));
+        request->TargetName.Buffer = buffer;
+        request->TicketFlags = static_cast<ULONG>(flags);
+        request->CacheOptions = static_cast<ULONG>(options);
+        request->EncryptionType = static_cast<ULONG>(type);
+        RETRIEVE_TKT_RESPONSE* response{ nullptr };
+        auto result{ CallPackage(requestBytes.data(), requestBytes.size(), &response) };
+        if (result) {
+            auto& ticket{ response->Ticket };
+            std::wcout << "ServiceName         : " << ticket.ServiceName << std::endl;
+            std::wcout << "TargetName          : " << ticket.TargetName << std::endl;
+            std::wcout << "ClientName          : " << ticket.ClientName << std::endl;
+            std::wcout << "DomainName          : " << ticket.DomainName.Buffer << std::endl;
+            std::wcout << "TargetDomainName    : " << ticket.TargetDomainName.Buffer << std::endl;
+            std::wcout << "AltTargetDomainName : " << ticket.AltTargetDomainName.Buffer << std::endl;
+            std::wcout << "SessionKey.KeyType  : " << ticket.SessionKey.KeyType << std::endl;
+            std::wcout << "SessionKey.Value    : ";
+            HexDecode(std::cout, std::wstring{ ticket.SessionKey.Value, ticket.SessionKey.Value + ticket.SessionKey.Length });
+            std::wcout << std::endl;
+            std::wcout << "TicketFlags         : " << ticket.TicketFlags << std::endl;
+            std::wcout << "Flags               : " << ticket.Flags << std::endl;
+            std::wcout << "KeyExpirationTime   : " << ticket.KeyExpirationTime.QuadPart << std::endl;
+            std::wcout << "StartTime           : " << ticket.StartTime.QuadPart << std::endl;
+            std::wcout << "EndTime             : " << ticket.EndTime.QuadPart << std::endl;
+            std::wcout << "RenewUntil          : " << ticket.RenewUntil.QuadPart << std::endl;
+            std::wcout << "TimeSkew            : " << ticket.TimeSkew.QuadPart << std::endl;
+            std::wcout << "EncodedTicket       : ";
+            HexDecode(std::cout, std::wstring{ ticket.EncodedTicket, ticket.EncodedTicket + ticket.EncodedTicketSize });
+            std::wcout << std::endl;
+            LsaFreeReturnBuffer(response);
+        }
+    }
+
     template<typename _Request, typename _Response>
     bool Proxy::CallPackage(const _Request& submitBuffer, _Response** returnBuffer) const {
         if (lsa->Connected()) {
