4258c696900b18fa5f5a77e43d94e59c5fa7149d
Evan McBroom <15305789+EvanMcBroom@users.noreply.github.com>
2023-11-17 18:35:34 -0500

    Added support for running commands directly

--- source/main.cpp
+++ source/main.cpp
@@ -15,6 +15,12 @@
 #include <wininet.h>
 
 namespace {
+    // https://gist.github.com/EvanMcBroom/2a9bed888c2755153a9616aa7ae1f79a
+    template<typename _T>
+    unsigned int constexpr Hash(_T const* input) {
+        return *input ? static_cast<unsigned int>(*input) + 33 * Hash(input + 1) : 5381;
+    }
+
     void Help(Cli& cli, const std::string& args) {
         std::cout << "Please refer to the wiki for information about specific commands:" << std::endl
                   << "https://github.com/EvanMcBroom/lsa-whisperer/wiki" << std::endl;
@@ -107,11 +113,13 @@ int main(int argc, char** argv) {
     bool noHistory{ false };
     bool showHelp{ false };
     std::string historyFile{ "./.lsa_history.txt" };
+    std::vector<std::string> commands;
     // clang-format off
     auto args = (
         clipp::option("-h", "--help").set(showHelp).doc("Show this help message."),
         clipp::option("--history-file").doc("Specify an alternative command line history file.") & clipp::value("path", historyFile),
-        clipp::option("--no-history").set(noHistory).doc("Do not create an appcontainer process for cloudap support.")
+        clipp::option("--no-history").set(noHistory).doc("Do not create an appcontainer process for cloudap support."),
+        clipp::opt_values("command", commands)
     );
     // clang-format on
     clipp::parse(argc, argv, args);
@@ -122,44 +130,71 @@ int main(int argc, char** argv) {
     if (noHistory) {
         historyFile.clear();
     }
-
-    Cli cli{ historyFile };
     auto lsa{ std::make_shared<Lsa>(std::cout) };
+    if (!commands.empty()) {
+        // Process each commands
+        for (auto& command : commands) {
+            std::istringstream inputStream{ command };
+            std::vector<std::string> tokens;
+            std::copy(std::istream_iterator<std::string>(inputStream), std::istream_iterator<std::string>(), std::back_inserter(tokens));
+            // Construct an equivalent to argv
+            std::vector<char*> argv;
+            std::for_each(tokens.begin(), tokens.end(), [&argv](const std::string& arg) {
+                argv.push_back(const_cast<char*>(arg.data()));
+            });
+            // Pass the command to the appropriate handler
+            switch (Hash(argv[0])) {
+            case Hash("cloudap"): Cloudap::Call(lsa, argv); break;
+            case Hash("kerberos"): Kerberos::Call(lsa, argv); break;
+            case Hash("live"): Live::Call(lsa, argv); break;
+            case Hash("msv1_0"): Msv1_0::Call(lsa, argv); break;
+            case Hash("negotiate"): Negotiate::Call(lsa, argv); break;
+            case Hash("pku2u"): Pku2u::Call(lsa, argv); break;
+            case Hash("schannel"): Schannel::Call(lsa, argv); break;
+            case Hash("spm"): Spm::Call(lsa, argv); break;
+            default:
+                break;
+            }
+        }
+    } else {
+        // Start repl shell
+        Cli cli{ historyFile };
 
-    cli.AddCommand(".clear", [](Cli& cli, const std::string& args) {
-        cli.clear_screen();
-    });
-    cli.AddCommand(".help", Help);
-    cli.AddCommand(".history", History);
-    cli.AddCommand(".nonce", Nonce);
-    cli.AddCommand(".token", Token::Command);
-    cli.AddCommand("cloudap", CommandFactory(lsa, Cloudap::Call));
-    cli.AddCommand("kerberos", CommandFactory(lsa, Kerberos::Call));
-    cli.AddCommand("live", CommandFactory(lsa, Live::Call));
-    cli.AddCommand("msv1_0", CommandFactory(lsa, Msv1_0::Call));
-    cli.AddCommand("negotiate", CommandFactory(lsa, Negotiate::Call));
-    cli.AddCommand("pku2u", CommandFactory(lsa, Pku2u::Call));
-    cli.AddCommand("schannel", CommandFactory(lsa, Schannel::Call));
-    cli.AddCommand("spm", CommandFactory(lsa, Spm::Call));
-    cli.AddExitCommand(".exit");
-    cli.AddExitCommand(".quit");
-    // Add autocompletions for each command's subcommands
-    auto cloudapPluginFunctions{ magic_enum::enum_names<Cloudap::Aad::CALL>() };
-    auto cloudapMessages(magic_enum::enum_names<Cloudap::PROTOCOL_MESSAGE_TYPE>());
-    std::vector<std::string> cloudapSubCommands{ cloudapMessages.begin(), cloudapMessages.end() };
-    cloudapSubCommands.insert(cloudapSubCommands.end(), cloudapPluginFunctions.begin(), cloudapPluginFunctions.end());
-    cloudapSubCommands.erase(std::remove(cloudapSubCommands.begin(), cloudapSubCommands.end(), "CreateBindingKey"), cloudapSubCommands.end());
-    cloudapSubCommands.erase(std::remove(cloudapSubCommands.begin(), cloudapSubCommands.end(), "GenerateBindingClaims"), cloudapSubCommands.end());
-    cli.AddSubCommandCompletions("cloudap", cloudapSubCommands);
-    cli.AddSubCommandCompletions("kerberos", SubCommands<Kerberos::PROTOCOL_MESSAGE_TYPE>());
-    cli.AddSubCommandCompletions("live", SubCommands<Live::PROTOCOL_MESSAGE_TYPE>());
-    cli.AddSubCommandCompletions("msv1_0", SubCommands<Msv1_0::PROTOCOL_MESSAGE_TYPE>());
-    cli.AddSubCommandCompletions("negotiate", SubCommands<Negotiate::PROTOCOL_MESSAGE_TYPE>());
-    cli.AddSubCommandCompletions("pku2u", SubCommands<Pku2u::PROTOCOL_MESSAGE_TYPE>());
-    cli.AddSubCommandCompletions("schannel", SubCommands<Schannel::PROTOCOL_MESSAGE_TYPE>());
-    cli.AddSubCommandCompletions("spm", SubCommands<SpmApi::NUMBER>());
-    cli.AddSubCommandCompletions(".token", SubCommands<Token::SubCommands>());
+        cli.AddCommand(".clear", [](Cli& cli, const std::string& args) {
+            cli.clear_screen();
+        });
+        cli.AddCommand(".help", Help);
+        cli.AddCommand(".history", History);
+        cli.AddCommand(".nonce", Nonce);
+        cli.AddCommand(".token", Token::Command);
+        cli.AddCommand("cloudap", CommandFactory(lsa, Cloudap::Call));
+        cli.AddCommand("kerberos", CommandFactory(lsa, Kerberos::Call));
+        cli.AddCommand("live", CommandFactory(lsa, Live::Call));
+        cli.AddCommand("msv1_0", CommandFactory(lsa, Msv1_0::Call));
+        cli.AddCommand("negotiate", CommandFactory(lsa, Negotiate::Call));
+        cli.AddCommand("pku2u", CommandFactory(lsa, Pku2u::Call));
+        cli.AddCommand("schannel", CommandFactory(lsa, Schannel::Call));
+        cli.AddCommand("spm", CommandFactory(lsa, Spm::Call));
+        cli.AddExitCommand(".exit");
+        cli.AddExitCommand(".quit");
+        // Add autocompletions for each command's subcommands
+        auto cloudapPluginFunctions{ magic_enum::enum_names<Cloudap::Aad::CALL>() };
+        auto cloudapMessages(magic_enum::enum_names<Cloudap::PROTOCOL_MESSAGE_TYPE>());
+        std::vector<std::string> cloudapSubCommands{ cloudapMessages.begin(), cloudapMessages.end() };
+        cloudapSubCommands.insert(cloudapSubCommands.end(), cloudapPluginFunctions.begin(), cloudapPluginFunctions.end());
+        cloudapSubCommands.erase(std::remove(cloudapSubCommands.begin(), cloudapSubCommands.end(), "CreateBindingKey"), cloudapSubCommands.end());
+        cloudapSubCommands.erase(std::remove(cloudapSubCommands.begin(), cloudapSubCommands.end(), "GenerateBindingClaims"), cloudapSubCommands.end());
+        cli.AddSubCommandCompletions("cloudap", cloudapSubCommands);
+        cli.AddSubCommandCompletions("kerberos", SubCommands<Kerberos::PROTOCOL_MESSAGE_TYPE>());
+        cli.AddSubCommandCompletions("live", SubCommands<Live::PROTOCOL_MESSAGE_TYPE>());
+        cli.AddSubCommandCompletions("msv1_0", SubCommands<Msv1_0::PROTOCOL_MESSAGE_TYPE>());
+        cli.AddSubCommandCompletions("negotiate", SubCommands<Negotiate::PROTOCOL_MESSAGE_TYPE>());
+        cli.AddSubCommandCompletions("pku2u", SubCommands<Pku2u::PROTOCOL_MESSAGE_TYPE>());
+        cli.AddSubCommandCompletions("schannel", SubCommands<Schannel::PROTOCOL_MESSAGE_TYPE>());
+        cli.AddSubCommandCompletions("spm", SubCommands<SpmApi::NUMBER>());
+        cli.AddSubCommandCompletions(".token", SubCommands<Token::SubCommands>());
 
-    cli.Start();
+        cli.Start();
+    }
     return 0;
 }
